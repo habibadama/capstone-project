669a0d73dff66e046b4590682882c3c16185a375
garbaali226@gmail.com <habibadamadioumanera@gmail.com>
2025-09-01 11:59:03 +0000

    Mise en place des APIs

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -14,9 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from restaurant.views import BookingViewSet
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter 
+from restaurant.views import BookingViewSet 
+
+router = DefaultRouter()
+router.register(r'tables', BookingViewSet)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('restaurant.urls')),
+    path('restaurant/', include('restaurant.urls')),
+    path('restaurant/booking/', include(router.urls)),
 ]
--- restaurant/models.py
+++ restaurant/models.py
@@ -2,16 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Reservation(models.Model):
-    name = models.CharField(max_length=255)
-    no_of_guests = models.IntegerField()
-    bookingDate = models.DateTimeField()
- 
-
-    def __str__(self):
-        return f"Reservation for {self.name} on {self.bookingDate} for {self.no_of_guests} guests"
-    
-
 class Menu(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2) 
@@ -19,3 +9,13 @@ class Menu(models.Model):
 
     def __str__(self):
         return f"{self.title} - ${self.price} (Inventory: {self.inventory})"
+    
+    
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField()
+    bookingDate = models.DateTimeField()
+ 
+
+    def __str__(self):
+        return f"Booking for {self.name} on {self.bookingDate} for {self.no_of_guests} guests"
